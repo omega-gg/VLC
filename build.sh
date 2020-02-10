571c6df590fcf300548d1f1d95b7536ee8a7c9e2
bunjee <bunjee@omega.gg>
2020-02-10 14:04:33 +0100

    build: Update script (3)

--- build.sh
+++ build.sh
@@ -86,11 +86,7 @@ fi
 echo ""
 echo "GET DEPENDENCIES"
 
-sudo cp /etc/apt/sources.list /etc/apt/sources.list~
-sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
-sudo apt-get update
-
-sudo apt-get build-dep vlc
+sudo apt-get -y build-dep vlc
 
 #--------------------------------------------------------------------------------------------------
 # Build
